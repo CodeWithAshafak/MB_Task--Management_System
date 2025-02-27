26fdc4afbc4e5867b98fc2eea2238765d8c97584
CodeWithAshafak <ashafak04@gmail.com>
2025-02-27 19:42:53 +0530

    deployed

--- frontend/src/Config.jsx
+++ frontend/src/Config.jsx
@@ -1,5 +1,4 @@
 // https://mb-task-management-system-1.onrender.com
-
- const BASE_URL= "https://mb-task-management-system-1.onrender.com"
+const BASE_URL="https://mb-task-management-system-1.onrender.com"
 
 export default BASE_URL
