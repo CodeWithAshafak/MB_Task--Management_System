9f517e709b002b3d13bc6962137adeb6094232ab
CodeWithAshafak <ashafak04@gmail.com>
2025-02-27 19:29:03 +0530

    baseUrl Added

--- frontend/src/Config.jsx
+++ frontend/src/Config.jsx
@@ -1,5 +1,5 @@
 // https://mb-task-management-system-1.onrender.com
 
-BASE_URL= "https://mb-task-management-system-1.onrender.com"
+ const BASE_URL= "https://mb-task-management-system-1.onrender.com"
 
 export default BASE_URL
